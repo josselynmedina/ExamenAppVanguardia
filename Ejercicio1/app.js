01820ed7f3242e4771c3eab8908794cc4073fbdc
josselynmedina95 <josselynmedina951610@icloud.com>
2018-11-09 00:01:40 -0600

    ejercicio1

---
+++ Ejercicio1/app.js
@@ -0,0 +1,24 @@
+const fs= require ('fs');
+const _=require('lodash');
+const yarg = require('yargs').argv;
+
+var p1 = "Josselyn";
+var p2 = "Medina";
+var p3 = "21311021";
+var p = p1 +p2;
+
+//let p1 = {"Nombre": "Josselyn"};
+//let p2 = {"Apellido": "Medina"};
+//let p3 = {"cuenta": "21311021"};
+
+//let res = _.assign(p1, p2);
+
+fs.appendFile('./Examen1.txt', 'Primer Examen Desarrollo de Applicaciones de Vanguardia.' + " " + p + " "+ p3 , error => {
+  if (error)
+    console.log(error);
+  else
+    console.log('El texto fue creado');
+});
+
+console.log(p1,p2,p3);
+console.log('última línea del programa');
